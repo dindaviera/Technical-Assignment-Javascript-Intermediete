47f53fa55ba67066b613890d1f19a085cb8d2edb
dindaviera <90680400+dindaviera@users.noreply.github.com>
2021-10-11 19:17:15 +0700

    update-jawaban

--- 01-Array-01.js
+++ 01-Array-01.js
@@ -1,4 +1,4 @@
-//Soal-01
+//Soal-01-array
 const arr = [1, 2, 3];
 const arr2 = [8, 1, 5, 7];
 
@@ -23,5 +23,7 @@ const newArr2 = reverse(arr2);
 
 console.log(arr, newArr);
 console.log(arr2, newArr2);
+
+
 
  
