08c9683adf161d9479c8280d26c3a873c96895a1
dindaviera <90680400+dindaviera@users.noreply.github.com>
2021-10-11 19:15:33 +0700

    update

---
+++ 01-Array-01.js
@@ -0,0 +1,27 @@
+//Soal-01
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
+
+function reverse(arr) {
+  let panjangArr = arr.length - 1;
+  let newArr = [];
+  for (i = panjangArr; i >= 0; i--){
+    newArr.push(arr[i]);
+  }
+  return newArr;
+
+  let panjangArr2 = arr2.length - 1;
+  let newArr2 =  [];
+  for (i = panjangArr2; i >=0; i--){
+    newArr.push(arr2[i]);
+  }
+  return newArr2;
+}
+
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
+
+console.log(arr, newArr);
+console.log(arr2, newArr2);
+
+ 
